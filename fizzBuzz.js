98213884a56bb3f7af26e0325a0cc9998b165efc
Mwesigye Nicholas <nicholastoshack123451111@gmail.com>
2024-04-06 09:11:31 -0400

    FizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,28 @@
+/**
+ * fizzBuzz - prints FizzBuzz for numbers 
+ * divisible 3 and 5, and Fizz for numbers
+ * divivble by 3 only and Buzz for numbers 
+ * divisible by 5 only
+ * numbers are between 1 to 100
+ *
+ * Returns: 0
+ */
+
+const fizzBuzz = () => {
+    for (let i = 1; i <= 100; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0){
+            console.log("Fizz");
+        }
+        else if (i % 5 == 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+    return 0;
+}
+fizzBuzz();
